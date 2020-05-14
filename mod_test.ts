37e97f87dc940d2cdb75f61fff4b478ec592dd7d
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 04:25:15 +0530

    :white_check_mark: Add tests

--- mod_test.ts
+++ mod_test.ts
@@ -1,6 +1,12 @@
 import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
-import { mode } from "./mod.ts";
+import { checkIfNameIsAvailable } from "./mod.ts";
 
-Deno.test("test starter function", async (): Promise<void> => {
-  assertEquals(mode(), 0);
+Deno.test("name should be available", async (): Promise<void> => {
+  assertEquals(await checkIfNameIsAvailable("abc"), true);
+});
+Deno.test("name should not be available", async (): Promise<void> => {
+  assertEquals(
+    await checkIfNameIsAvailable(`3hu92jr031-${Math.random().toString()}`),
+    false,
+  );
 });
