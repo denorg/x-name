2e7f07cfcf88a79d83f452850e55f93c5786b858
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 04:23:39 +0530

    :sparkles: Add module

--- mod.ts
+++ mod.ts
@@ -1,4 +1,19 @@
-/** JSDoc for this line */
-export function mode() {
-  return 0;
+const DATABASE_URL =
+  "https://raw.githubusercontent.com/denoland/deno_website2/master/database.json";
+
+let database: {
+  [index: string]: {
+    type: "github" | "gitlab" | "deno_std";
+    owner?: string;
+    repo?: string;
+    desc?: string;
+  };
+} | undefined = undefined;
+
+/** Check whether a package name is available in deno.land/x */
+export async function checkIfNameIsAvailable(name: string) {
+  if (!database) {
+    database = await (await fetch(DATABASE_URL)).json();
+  }
+  return !!(database && database[name]);
 }
