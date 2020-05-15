a674c72a4479ecd18769150d24014fc43a474378
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 16:01:43 +0530

    :bug: Fix empty database check

--- mod.ts
+++ mod.ts
@@ -5,7 +5,7 @@ let database: string[] = [];
 
 /** Check whether a package name is available in deno.land/x */
 export async function checkIfNameIsAvailable(name: string) {
-  if (!database) {
+  if (!database.length) {
     database = Object.keys(await (await fetch(DATABASE_URL)).json());
   }
   return database.includes(name);
