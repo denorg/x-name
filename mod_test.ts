dee355e5b568e8eb7575501b105357c255a509ba
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 21:19:11 +0530

    :white_check_mark: Update tests with desired result

--- mod_test.ts
+++ mod_test.ts
@@ -2,11 +2,11 @@ import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
 import { checkIfNameIsAvailable } from "./mod.ts";
 
 Deno.test("name should be available", async (): Promise<void> => {
-  assertEquals(await checkIfNameIsAvailable("abc"), true);
+  assertEquals(await checkIfNameIsAvailable("abc"), false);
 });
 Deno.test("name should not be available", async (): Promise<void> => {
   assertEquals(
     await checkIfNameIsAvailable(`3hu92jr031-${Math.random().toString()}`),
-    false,
+    true,
   );
 });
