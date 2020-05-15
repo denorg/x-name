82f0d6cc9a6882e6f35e8e8b2e175f6711c292ee
Anand Chowdhary <mail@anandchowdhary.com>
2020-05-15 15:54:49 +0530

    :recycle: Only store project keys in memory

--- mod.ts
+++ mod.ts
@@ -1,19 +1,12 @@
 const DATABASE_URL =
   "https://raw.githubusercontent.com/denoland/deno_website2/master/database.json";
 
-let database: {
-  [index: string]: {
-    type: "github" | "gitlab" | "deno_std";
-    owner?: string;
-    repo?: string;
-    desc?: string;
-  };
-} | undefined = undefined;
+let database: string[] = [];
 
 /** Check whether a package name is available in deno.land/x */
 export async function checkIfNameIsAvailable(name: string) {
   if (!database) {
-    database = await (await fetch(DATABASE_URL)).json();
+    database = Object.keys(await (await fetch(DATABASE_URL)).json());
   }
-  return !!(database && database[name]);
+  return database.includes(name);
 }
